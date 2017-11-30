95ad5abc5a2bbda17e0b28e5ccd3634e9a7df1e4
Hiep Phan <tabb.phan@gmail.com>
2017-11-30 13:25:57 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/', function(req, res) {
+   res.render('home',{isSearching:false}); 
+});
+
+app.post('/searchmovie', function(req, res) {
+   var url = 'http://www.omdbapi.com/?apikey=thewdb&';
+   var value = req.body.text_input;
+   var searchMethod = req.body.result_num;
+   var searchForOne;
+   if (searchMethod == "one") {
+       searchForOne = true;
+       url += 't=' + value;
+   }
+   else {
+       searchForOne = false;
+       url += 's=' + value;
+   }
+    
+   request(url, function(error, response, body) {
+      var data = JSON.parse(body);
+      if (!error && response.statusCode == 200) {
+          res.render('home', { isSearching:true, searchForOne:searchForOne, result:data });
+      }
+      else {
+          res.send('There was an error.');
+      }
+   });
+});
+
+app.listen(process.env.PORT,process.env.IP, function() {
+    console.log('Server is running...');
+});
