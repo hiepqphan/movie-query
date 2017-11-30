172353010da88cf4c891453ffe16a20d433c0777
Hiep Phan <tabb.phan@gmail.com>
2017-11-30 13:59:16 -0500

    added ejs

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.post('/searchmovie', function(req, res) {
    });
 });
 
-app.listen(process.env.PORT,process.env.IP, function() {
+var PORT = process.env.PORT || 5000;
+
+app.listen(PORT, function() {
     console.log('Server is running...');
 });
